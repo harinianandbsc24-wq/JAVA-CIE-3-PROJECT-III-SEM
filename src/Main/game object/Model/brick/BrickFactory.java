c34b5d749054efa6d1a915c2d4a2f70e24b64976
Harini Anand <harinianandbsc24@rvu.edu.in>
2025-11-10 14:09:31 +0530

    Update BrickFactory.java

--- src/Main/game object/Model/brick/BrickFactory.java
+++ src/Main/game object/Model/brick/BrickFactory.java
@@ -6,57 +6,49 @@ import java.awt.Point;
 import main.java.gameObjects.controller.BrickController;
 
 /**
- * This class implements the Factory design pattern to create brick objects
+ * Factory class to create Brick objects following the Factory design pattern.
+ * It supports multiple Brick types for extensibility.
  * 
  * @author Emily
- *
  */
-
 public class BrickFactory {
 
-	/**
-	 * Default constructor
-	 */
-	
-	public BrickFactory() {
+    /**
+     * Default constructor
+     */
+    public BrickFactory() {
+        // No special initialization needed currently
+    }
 
-	}
-
-	/**
-	 * Factory method to create brick objects
-	 * 
-	 * @param point The point of the brick object
-	 * @param size  The size of the brick object
-	 * @param type  The type of the brick object
-	 * @return A brick object
-	 */
-
-	public BrickController makeBrick(Point point, Dimension size, BrickType type) {
-		BrickController out;
-		switch (type) {
-		case CLAY:
-			out = new ClayBrick(point, size);
-			break;
-		case STEEL:
-			out = new SteelBrick(point, size);
-			break;
-		case CEMENT:
-			out = new CementBrick(point, size);
-			break;
-		case SPECIAL:
-			out = new SpecialBrick(point, size);
-			break;
-		case VIBRANIUM:
-			out = new VibraniumBrick(point, size);
-			break;
-		case HEALTH:
-			out = new HealthBrick(point, size);
-			break;
-		default:
-			throw new IllegalArgumentException(String.format("Unknown Type:%d\n", type));
-
-		}
-		return out;
-	}
-
+    /**
+     * Factory method to create brick objects of specified type, position, and size.
+     *
+     * @param point The top-left point of the brick
+     * @param size  The dimension (width and height) of the brick
+     * @param type  The BrickType enum specifying the kind of brick
+     * @return A BrickController instance of the specified type
+     * @throws IllegalArgumentException if an unknown BrickType is specified
+     */
+    public BrickController makeBrick(Point point, Dimension size, BrickType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("BrickType cannot be null");
+        }
+        
+        switch (type) {
+            case CLAY:
+                return new ClayBrick(point, size);
+            case STEEL:
+                return new SteelBrick(point, size);
+            case CEMENT:
+                return new CementBrick(point, size);
+            case SPECIAL:
+                return new SpecialBrick(point, size);
+            case VIBRANIUM:
+                return new VibraniumBrick(point, size);
+            case HEALTH:
+                return new HealthBrick(point, size);
+            default:
+                throw new IllegalArgumentException("Unknown BrickType: " + type);
+        }
+    }
 }
