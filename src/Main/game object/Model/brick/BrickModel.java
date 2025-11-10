a6e7b9cdbdc1094f00b466dae24df16b182e4864
Harini Anand <harinianandbsc24@rvu.edu.in>
2025-11-10 14:12:01 +0530

    Update BrickModel.java

--- src/Main/game object/Model/brick/BrickModel.java
+++ src/Main/game object/Model/brick/BrickModel.java
@@ -4,172 +4,94 @@ import java.awt.*;
 import java.util.Random;
 
 /**
- * This class stores the application data for brickController class
+ * This class stores the application data for BrickController class
  * 
  * @author Emily
- *
  */
-
 public class BrickModel {
 
-	private int fullStrength;
-	private int strength;
-	protected static Random rnd;
+    private int fullStrength;
+    private int strength;
+    private static final Random rnd = new Random(); // Fixed static initialization
 
-	private String name;
-	private boolean broken;
-	Shape brickFace;
+    private String name;
+    private boolean broken;
+    private Shape brickFace;
 
-	private Color border;
-	private Color inner;
+    private final Color border;
+    private final Color inner;
 
-	/**
-	 * Default constructor to initialse the default value for the fields and
-	 * parameters
-	 * 
-	 * @param name     Name of the brick object
-	 * @param strength Strength of the brick object
-	 */
+    /**
+     * Constructor to initialize all fields of the BrickModel.
+     * 
+     * @param name     Name of the brick object
+     * @param strength Strength of the brick object
+     * @param border   Border color of the brick
+     * @param inner    Inner color of the brick
+     */
+    public BrickModel(String name, int strength, Color border, Color inner) {
+        this.name = name;
+        this.fullStrength = this.strength = strength;
+        this.border = border;
+        this.inner = inner;
+        this.broken = false;
+    }
 
-	public BrickModel(String name, int strength, Color border, Color inner) {
+    /**
+     * Random generator for use in all BrickModel instances
+     * 
+     * @return Random instance
+     */
+    public static Random getRandom() {
+        return rnd;
+    }
 
-		setBroken(false);
-		setRnd(new Random());
-		this.setName(name);
-		this.fullStrength = this.strength = strength;
+    // Getters and setters
 
-		this.border = border;
-		this.inner = inner;
+    public String getName() {
+        return name;
+    }
 
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * Setter to set the random value
-	 * 
-	 * @param rnd random value
-	 */
+    public Shape getBrickFace() {
+        return brickFace;
+    }
 
-	public static void setRnd(Random rnd) {
-		BrickModel.rnd = rnd;
-	}
+    public void setBrickFace(Shape brickFace) {
+        this.brickFace = brickFace;
+    }
 
-	/**
-	 * Getter to get the random value
-	 * 
-	 * @return rnd random value
-	 */
+    public int getFullStrength() {
+        return fullStrength;
+    }
 
-	public static Random getRnd() {
-		return rnd;
-	}
+    // No setter for fullStrength - it should be final or set only in constructor
 
-	/**
-	 * Getter to get the name of the brick
-	 * 
-	 * @return String brick name
-	 */
+    public int getStrength() {
+        return strength;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
 
-	/**
-	 * Setter to set the name
-	 * 
-	 * @param name String name
-	 */
+    public boolean isBroken() {
+        return broken;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setBroken(boolean broken) {
+        this.broken = broken;
+    }
 
-	/**
-	 * Getter to get the brickFace
-	 * 
-	 * @return the shape of brickFace
-	 */
+    public Color getBorderColor() {
+        return border;
+    }
 
-	public Shape getBrickFace() {
-		return brickFace;
-	}
-
-	/**
-	 * Getter to get the full strength of the brick
-	 * 
-	 * @return The full strength integer of the brick
-	 */
-
-	public int getFullStrength() {
-		return fullStrength;
-	}
-
-	/**
-	 * Setter to set the full strength of the brick
-	 * 
-	 * @param fullStrength The integer full Strength
-	 */
-
-	public void setFullStrength(int fullStrength) {
-		this.fullStrength = fullStrength;
-	}
-
-	/**
-	 * Getter to get the strength of the brick
-	 * 
-	 * @return The strength integer of the brick
-	 */
-
-	public int getStrength() {
-		return strength;
-	}
-
-	/**
-	 * Setter to set the strength of the brick
-	 * 
-	 * @param strength The integer strength
-	 */
-
-	public void setStrength(int strength) {
-		this.strength = strength;
-	}
-
-	/**
-	 * Getter to get the current condition of the brick
-	 * 
-	 * @return True if the brick is broken, False is the brick is not broken
-	 */
-	public boolean isBroken() {
-		return broken;
-	}
-
-	/**
-	 * Setter to set the current condition of the brick
-	 * 
-	 * @param broken The boolean value of whether the brick is broken or not
-	 */
-
-	public void setBroken(boolean broken) {
-		this.broken = broken;
-	}
-
-	/**
-	 * Getter for the border color of the brick object
-	 * 
-	 * @return the color of the border of the brick object
-	 */
-
-	public Color getBorderColor() {
-		return border;
-	}
-
-	/**
-	 * Getter for the inner color of the brick object
-	 * 
-	 * @return the inner color of the brick object
-	 */
-
-	public Color getInnerColor() {
-		return inner;
-	}
-
+    public Color getInnerColor() {
+        return inner;
+    }
 }
+
