c18955dca1502b5c2d7043cdae82b15ba11ea9b5
Harini Anand <harinianandbsc24@rvu.edu.in>
2025-11-10 17:42:56 +0530

    Update Playable.java

--- src/Main/game object/Model/player/Playable.java
+++ src/Main/game object/Model/player/Playable.java
@@ -1,53 +1,45 @@
 package main.java.gameObjects.model.player;
 
-import java.awt.*;
+import java.awt.Point;
 
 /**
- * This interface allows other class to implement its methods
+ * Interface defining controllable player paddle movements.
+ * 
  * @author Emily
- *
  */
-
 public interface Playable {
-	
-	/**
-	 * Method to move the player's paddle
-	 */
-	
-	public void move();
+    
+    /**
+     * Moves the player's paddle by a default or previously set amount.
+     */
+    void move();
 
-	/**
-	 * Method to move the player's paddle a specific amount
-	 * 
-	 * @param m Move amount integer
-	 */
-	
-	public void move(int m);
-	
-	/**
-	 * Method to move the player's paddle to the left
-	 */
+    /**
+     * Moves the player's paddle by the specified amount.
+     * 
+     * @param distance The amount to move the paddle
+     */
+    void move(int distance);
 
-	public void moveLeft();
-	
-	/**
-	 * Method to move the player's paddle to the right
-	 */
+    /**
+     * Moves the paddle to the left.
+     */
+    void moveLeft();
 
-	public void moveRight();
-	
-	/**
-	 * Method to stop the player's paddle from moving
-	 */
+    /**
+     * Moves the paddle to the right.
+     */
+    void moveRight();
 
-	public void stop();
-	
-	/**
-	 * Method to move the player's paddle to a specific point
-	 * 
-	 * @param p The point of the player's paddle to move to
-	 */
+    /**
+     * Stops the paddle's movement.
+     */
+    void stop();
 
-	public void moveTo(Point p);
-
+    /**
+     * Moves the paddle to a specific point.
+     * 
+     * @param point The point to move the paddle to
+     */
+    void moveTo(Point point);
 }
