6a73f7c2246933e7fdaaa7cab351013dd490dbaa
Harini Anand <harinianandbsc24@rvu.edu.in>
2025-11-10 14:39:27 +0530

    Update ImpactDirection.java

--- src/Main/game object/Model/brick/ImpactDirection.java
+++ src/Main/game object/Model/brick/ImpactDirection.java
@@ -1,41 +1,36 @@
 package main.java.gameObjects.model.brick;
 
 /**
- * Different possible directions of impact
+ * Enum representing different possible directions of impact on bricks.
+ * 
  * @author Emily
- *
  */
-
 public enum ImpactDirection {
-	
-	/**
-	 *  Up Impact Direction
-	 */
-	
-	UP_IMPACT,
-	
-	/**
-	 *  Down Impact Direction
-	 */
-	
-	DOWN_IMPACT,
-	
-	/**
-	 *  Left Impact Direction
-	 */
-	
-	LEFT_IMPACT,
-	
-	/**
-	 *  Right Impact Direction
-	 */
-	
-	RIGHT_IMPACT,
-	
-	/**
-	 *  No Impact Made
-	 */
-	
-	NO_IMPACT
-}
 
+    /**
+     * Up Impact Direction
+     */
+    UP_IMPACT,
+
+    /**
+     * Down Impact Direction
+     */
+    DOWN_IMPACT,
+
+    /**
+     * Left Impact Direction
+     */
+    LEFT_IMPACT,
+
+    /**
+     * Right Impact Direction
+     */
+    RIGHT_IMPACT,
+
+    /**
+     * No Impact has been made
+     */
+    NO_IMPACT;
+
+    // Optional: You can add methods here if needed, such as for reverse direction or utility
+}
