3a369e05a1700b7781a2aaa4003733f0acf0da82
Harini Anand <harinianandbsc24@rvu.edu.in>
2025-11-10 14:53:15 +0530

    Refactor CrackModel to use CrackDirection enum

--- src/Main/game object/Model/crack/CrackModel.java
+++ src/Main/game object/Model/crack/CrackModel.java
@@ -1,68 +1,68 @@
 package main.java.gameObjects.model.crack;
 
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.Shape;
-import java.awt.geom.GeneralPath;
-import java.awt.geom.Point2D;
-import java.util.Random;
-
-import main.java.gameObjects.model.brick.BrickModel;
-
 /**
- * Objects of this class represent the cracks of the brick
+ * Enum representing the possible directions of cracks.
+ * Includes specific directions and orientations for convenience.
  * 
  * @author Emily
- *
  */
+public enum CrackDirection {
+    
+    /** Left crack direction */
+    LEFT,
+    
+    /** Right crack direction */
+    RIGHT,
+    
+    /** Up crack direction */
+    UP,
+    
+    /** Down crack direction */
+    DOWN,
+    
+    /** Vertical crack direction */
+    VERTICAL,
+    
+    /** Horizontal crack direction */
+    HORIZONTAL;
 
-public class CrackModel {
+    /**
+     * Returns the opposite crack direction where applicable.
+     * For example, LEFT's opposite is RIGHT, UP's opposite is DOWN,
+     * VERTICAL's opposite is HORIZONTAL, etc.
+     * 
+     * @return The opposite CrackDirection, or null if no opposite exists.
+     */
+    public CrackDirection opposite() {
+        switch (this) {
+            case LEFT:      return RIGHT;
+            case RIGHT:     return LEFT;
+            case UP:        return DOWN;
+            case DOWN:      return UP;
+            case VERTICAL:  return HORIZONTAL;
+            case HORIZONTAL:return VERTICAL;
+            default:        return null;
+        }
+    }
 
-	public final int CRACK_SECTIONS = 3;
-	public final double JUMP_PROBABILITY = 0.7;
+    /**
+     * Checks if this crack direction is considered vertical.
+     * Includes UP, DOWN, and VERTICAL as vertical.
+     * 
+     * @return True if vertical-oriented; false otherwise.
+     */
+    public boolean isVertical() {
+        return this == UP || this == DOWN || this == VERTICAL;
+    }
 
-	protected static Random rnd;
-
-	private int crackDepth;
-	private int steps;
-
-	/**
-	 * Constructer of crack class
-	 * 
-	 * @param crackDepth The depth of the crack
-	 * @param steps      The steps of the crack
-	 */
-
-	public CrackModel(int crackDepth, int steps) {
-
-		this.crackDepth = crackDepth;
-		this.steps = steps;
-
-	}
-	
-	/**
-	 * Getter to get the crack depth
-	 * 
-	 * @return crack depth
-	 */
-
-	public int getCrackDepth() {
-		return crackDepth;
-	}
-
-	/**
-	 * Getter to get the steps
-	 * 
-	 * @return steps
-	 */
-	
-	public int getSteps() {
-		return steps;
-	}
-
-
-
-	
-	
-
+    /**
+     * Checks if this crack direction is considered horizontal.
+     * Includes LEFT, RIGHT, and HORIZONTAL as horizontal.
+     * 
+     * @return True if horizontal-oriented; false otherwise.
+     */
+    public boolean isHorizontal() {
+        return this == LEFT || this == RIGHT || this == HORIZONTAL;
+    }
 }
+
