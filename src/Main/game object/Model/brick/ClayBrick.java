65b9bdbb4f1866fc3a2b74ad44743388d89e3105
Harini Anand <harinianandbsc24@rvu.edu.in>
2025-11-10 14:30:59 +0530

    Refactor ClayBrick class for clarity and consistency

--- src/Main/game object/Model/brick/ClayBrick.java
+++ src/Main/game object/Model/brick/ClayBrick.java
@@ -1,44 +1,39 @@
 package main.java.gameObjects.model.brick;
 
 import java.awt.*;
-import java.awt.Point;
-
 import main.java.gameObjects.controller.BrickController;
 
 /**
- * Objects of this class inherits from the brickController class, creating clay
- * brick objects
+ * ClayBrick represents a brick object of type clay
+ * It inherits from BrickController and provides default colors and strength.
  * 
  * @author Emily
- *
  */
-
 public class ClayBrick extends BrickController {
 
-	private static final String NAME = "Clay Brick";
-	private static final Color DEF_INNER = new Color(178, 34, 34).darker();
-	private static final Color DEF_BORDER = Color.GRAY;
-	private static final int CLAY_STRENGTH = 1;
+    private static final String NAME = "Clay Brick";
+    private static final Color DEF_INNER = new Color(178, 34, 34).darker();
+    private static final Color DEF_BORDER = Color.GRAY;
+    private static final int CLAY_STRENGTH = 1;
 
-	/**
-	 * Constructor to create a brick object of the type clay
-	 * 
-	 * @param point The coordinates of the point of the clay brick
-	 * @param size  The size of the clay brick
-	 */
+    /**
+     * Constructor to create a clay brick at the specified position and size.
+     *
+     * @param point The top-left position of the brick
+     * @param size The dimensions of the brick
+     */
+    public ClayBrick(Point point, Dimension size) {
+        super(NAME, point, size, DEF_BORDER, DEF_INNER, CLAY_STRENGTH);
+    }
 
-	public ClayBrick(Point point, Dimension size) {
-		super(NAME, point, size, DEF_BORDER, DEF_INNER, CLAY_STRENGTH);
-	}
+    @Override
+    protected Shape makeBrickFace(Point pos, Dimension size) {
+        return new Rectangle(pos, size);
+    }
 
-	@Override
-	protected Shape makeBrickFace(Point pos, Dimension size) {
-		return new Rectangle(pos, size);
-	}
-
-	@Override
-	public Shape getBrick() {
-		return super.getBrickFace();
-	}
-
+    @Override
+    public Shape getBrick() {
+        return super.getBrickFace();
+    }
 }
+
